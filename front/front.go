18e8c46910131b0b53d3f00919f247ecf6fda9b4
wrq <pyq225@gmail.com>
2017-10-28 01:11:27 +0800

    update router

---
+++ front/front.go
@@ -0,0 +1,15 @@
+package front
+
+import "github.com/gin-gonic/gin"
+
+func Index(c *gin.Context) {
+	c.JSON(200, gin.H{"msg": "this is index"})
+}
+
+func UserHomePage(c *gin.Context) {
+	c.JSON(200, gin.H{"msg": "this is user homepage"})
+}
+
+func ArticleDetail(c *gin.Context) {
+	c.JSON(200, gin.H{"msg": "this is article detail"})
+}
--- router.go
+++ router.go
@@ -3,10 +3,15 @@ package main
 import (
 	"github.com/gin-gonic/gin"
 	. "backend/apis"
+	. "backend/front"
 )
 
 func initRouter() *gin.Engine {
 	router := gin.Default()
+
+	router.GET("/", Index)
+	router.GET("/u/:id", UserHomePage)
+	router.GET("/a/:id", ArticleDetail)
 
 	router.GET("/articles", GetArticlesAPI)
 	router.POST("/articles", AddArticleAPI)
