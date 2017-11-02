9a50d6193a3cfca61bebbe2e733d2159faffd9e2
wrq <pyq225@gmail.com>
2017-11-02 22:24:09 +0800

    update model

--- main.go
+++ main.go
@@ -2,9 +2,17 @@ package main
 
 import (
 	. "backend/commons"
+	"github.com/cihub/seelog"
+	"backend/models"
 )
 
 func main() {
+	db, err := models.InitDB()
+	if err != nil {
+		seelog.Critical("err open databases", err)
+		return
+	}
+	defer db.Close()
 	r := initRouter()
 	r.Run(HOST + PORT)
 }
--- models/models.go
+++ models/models.go
@@ -1,1 +1,35 @@
 package models
+
+import (
+	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/sqlite"
+)
+
+type Article struct {
+	gorm.Model
+	Title string
+	Body  string
+	//Tags      []*Tag `gorm:"-"` // tags of post
+}
+
+type User struct {
+	gorm.Model
+	Username  string
+	Password  string
+	GithubUrl string
+	IsAdmin   bool
+	AvatarUrl string
+}
+
+var DB *gorm.DB
+
+func InitDB() (*gorm.DB, error) {
+	db, err := gorm.Open("sqlite3", "test.db")
+	//db, err := gorm.Open("mysql", "root:mysql@/wblog?charset=utf8&parseTime=True&loc=Asia/Shanghai")
+	if err == nil {
+		DB = db
+		db.AutoMigrate(&Article{}, &User{})
+		return db, err
+	}
+	return nil, err
+}
